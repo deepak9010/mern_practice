17e68186ec05f846c3e4afb5f796c7d504f16b66
deepak9010 <deepak23072019@gmail.com>
2025-01-07 22:35:34 +0530

    currying, prototype,bubbling,capturing,debouncing

--- js_concept/index.js
+++ js_concept/index.js
@@ -368,3 +368,406 @@ printMyName();
 
 
 // console.log(my_array);
+
+
+/*********************************************** Currying ******************************************* */
+
+// currying in js
+// Currying is a function that takes one argument at a time and returns a new function expecting the next argument. It is a conversion of functions from callable as f(a,b,c)into callable as f(a)(b)(c).
+
+// It transform the function into a number of functions where every function will accept one argument.
+// by two types, first using bind and second using closure
+// example f(a,b) into f(a,b)
+
+// yt- roadsidecoder
+
+//        --------------------using closure--------------
+function f(a){
+  return function(b){
+      // console.log(a,b);
+      return `${a} ${b}`;
+  }
+}
+
+console.log(f(5)(6));
+
+
+// Basically Currying doesn’t call a function. It just transforms a function. They are constructed by chaining closures by immediately returning their inner functions simultaneously.
+/*Simple function*/ 
+const add = (a, b, c)=>{
+  return a+ b + c
+}
+console.log(add(1,2 ,3)); // 6
+
+/* Curried Function */
+const addCurry = (a) => { // takes one argument
+  return (b)=>{                 //takes second argument
+      return (c)=>{             //takes third argument
+          return a+b+c
+      }
+  }
+}
+console.log(addCurry(1)(2)(3)); //6
+
+
+
+
+// another example
+function evaluate(operation) {
+  return (a) => {
+      return (b) => {
+         if(operation === "sum") return a + b;
+         else if(operation === "multiply") return a * b;
+         else if(operation === "divide") return a / b;
+         else if(operation === "subtract") return a - b;
+         else return "No / Invalid Operation Selected"
+      }
+  }
+}
+
+
+console.log(evaluate("subtract")(5)(2));
+
+
+
+
+// exapmple of infinite currying 
+
+// infinite currying using simple straightfoward approach and it is time-taking solution
+// const sum = function(a) {
+//     return function(b) {
+//         return function(c) {
+//             return function(d) {
+//                 ...
+//                 ...
+//                 ...
+//                 return a + b + c + d + ... n;
+//             }
+//         }
+//     }
+// }
+
+// console.log(sum(a)(b)(c) ......n ())
+
+// The above solution is not much of an optimised solution.
+//  infinite currying using recursive
+
+  //recursive solution for infinte currying
+const sum = function(a) {
+  return function(b) {
+      if (b) {
+          return sum(a + b);
+      } else {
+          return a;
+      }
+  }
+}
+
+console.log(sum(2)(8)(9)(1)());   //in last add "()" to terminae the chain
+
+
+
+
+
+//  ---------using bind-----------
+// yt-namaste js
+
+
+let multiply= function(x,y){
+  console.log(x+y);
+}
+
+let multiplyByTwo = multiply.bind(this,2);
+multiplyByTwo(7);
+
+
+
+// Why is Currying useful in JavaScript?
+// It reduces the chances of error in our function by dividing it into multiple smaller functions that can handle one responsibility.
+//It is very useful in building modular and reusable code
+//It helps us to avoid passing the same variable multiple times
+//It makes the code more readable
+// It helps us to create a higher-order function
+
+
+
+
+
+
+
+
+
+
+
+
+
+/***************************************** Prototype Inheritance ********************************************************************* */
+// Prototype- basically , it is an hidden object which contain all the properties , methods to use for our original object, function.
+// yt- namaste js
+let arr1 = ["test" , "user"];
+
+let object = {
+  name:"test",
+  city:"noida",
+  getIntro: function(){
+      console.log(this.name + "from" +this.city);
+  }
+}
+
+let object2={
+  name:"test34"
+}
+
+// never use this proto in code directly
+object2.__proto__ = object;
+
+
+
+// doc link-  https://medium.com/@kevincennis/prototypal-inheritance-781bccc97edb
+// eg.
+function Dog() {
+
+ }
+Dog.prototype.bark = function() {
+  console.log("woof");
+ };
+ var fido = new Dog();
+fido.bark(); // ‘woof!’
+
+
+
+
+Function.prototype.mybind = function(){
+  console.log("usertest");
+}
+
+function fun2(){
+
+}
+
+
+
+
+// /**************************************** Event Bubbling and Capturing *********************************** */
+// yt- namstejs
+// doc.-> https://www.freecodecamp.org/news/event-bubbling-and-event-capturing-in-javascript/
+
+
+// Event flow in the DOM-> Events are generally processed in three phases in the DOM. They are the capturing, the target, and the bubbling phase.
+
+// Event capturing is the first phase , this phase trickles down from the top of the DOM tree to the target element.
+// Event Capturing also known as Event trickling-> trickling down(from grandparent to target)
+
+// The target phase is the second phase that begins immediately after the capturing phase ends. This phase is basically the end of the capturing and the beginning of the bubbling phase.The target element is the element where the event originally occurred.
+
+// Event bubbling is the third phase where the event bubbles up from the target element all the way to the global window object.
+// Event bubbling -> bubbling up(from target to grandparent) ,(o which div we are clicking then that div is target)
+// Event bubbling called bydefault means  useCapture is false bydefault
+
+// if useCapture(boolean value) is true then trickling down will happen, and if useCapture is false then bubbling up will happen 
+
+
+document.querySelector("#grandparent")
+.addEventListener('click', ()=>{
+  console.log("granparent clicked");
+}, true); //here this true is value of useCapture, if we donot pass any vlue then it is false bydefault
+
+
+document.querySelector("#parent")
+.addEventListener('click', ()=>{
+  console.log("parent clicked");
+}, true);
+
+document.querySelector("#child")
+.addEventListener('click', ()=>{
+  console.log("child clicked");
+}, true);
+
+// these bubbling and capturing are expensive for a application so we use  stopPropagation() to stop this flow/hierarchy
+//  below is the exmple of this
+ 
+document.querySelector("#grandparent")
+.addEventListener('click', (e)=>{
+  console.log("granparent clicked");
+}, true); 
+
+
+document.querySelector("#parent")
+.addEventListener('click', (e)=>{
+  console.log("parent clicked");
+  e.stopPropagation();
+}, true);
+
+document.querySelector("#child")
+.addEventListener('click', (e)=>{
+  console.log("child clicked");
+  e.stopPropagation();
+}, true);
+
+
+
+
+
+
+// /**************************************** Event Delegation *********************************** */
+//  Doc- https://www.freecodecamp.org/news/event-bubbling-and-event-capturing-in-javascript/
+// Event delegation - It takes advantage of the DOM (Document Object Model) bubbling event. This means setting event listeners on ancestor elements allows you to handle events efficiently. Unlike setting event listeners on individual elements that trigger the events. 
+// But there are some situations where it will be almost impossible to delegate to a child element. For example, it's impossible if the child has a nested child element itself. In this case, it will be impossible to delegate to a child element.
+
+document.querySelector("#category").addEventListener('click',(e)=>{
+  console.log(e.target.id);
+  console.log("category parent clicked");
+  // window.location.href = "/" + e.target.id;
+})
+
+
+
+
+
+
+
+/************************************************ Debouncing ******************************************************************** */
+
+// Doc- https://dev.to/jeetvora331/javascript-debounce-easiest-explanation--29hc
+// Doc- https://www.freecodecamp.org/news/deboucing-in-react-autocomplete-example/
+// Debouncing is a programming technique that helps to improve the performance of web applications by limiting the frequency of function calls.
+// Debouncing- Debouncing is a way of delaying the execution of a function until a certain amount of time has passed since the last time it was called. This can be useful for scenarios where we want to avoid unnecessary or repeated function calls that might be expensive or time-consuming.
+
+// eg- For example, imagine we have a search box that shows suggestions as the user types. If we call a function to fetch suggestions on every keystroke, we might end up making too many requests to the server, which can slow down the application and waste resources. Instead, we can use debouncing to wait until the user has stopped typing for a while before making the request.
+
+let counter = 0;
+const getData = () =>{
+  // calls an API and gets data
+  console.log('fetching data ....', counter++);
+}
+
+
+// Debouncing accepts a function and transforms it in to an updated (debounced) function so that the code inside the original function is executed after a certain period of time.
+// syntax
+// function debounce(func, delay) {
+//     return () => {}   // return debounced function
+// }
+
+// below we need to cleartimeout timer bcz here it is run after delay time but  suppose if we type 3 letter , now it will run after delay time but it will run 3 time, that's why we usenclearout time , using cleartimeout if we type some letter and pause then it run after delay time but it will only run 1 time.
+const debounce = function(func, delay){
+     let timer;
+     return function(){
+       let context = this,
+       args= arguments;
+       clearTimeout(timer);
+
+      timer = setTimeout(()=>{
+             getData.apply(context, arguments);
+          }, delay)
+     }
+ }
+const debouncedSearchData = debounce(getData, 2000);
+
+
+//   another example for debouncing- Button
+//   ques-1 - Create a button Ui and add debounce as follows->
+//          --> show "button pressed <X> times" every time button is pressed
+//          --> Increase "Triggered <Y> times" count after 800ms of debounce
+// roadsidecoder yt
+
+const btn = document.querySelector(".increment_btn");
+const btnPress = document.querySelector(".increment_pressed");
+const count = document.querySelector(".increment_count");
+
+var pressedCount =0;
+var triggerCount=0;
+
+const myDebounce = (cb,d) =>{
+    let timer;
+
+    return function(...args){
+        if(timer) clearTimeout(timer);
+        timer = setTimeout(()=>{
+           cb(...args);
+        },d)
+    }
+};
+
+const debouncedCount = myDebounce(()=>{
+      triggerCount += 1;
+      count.innerHTML = triggerCount;
+},800);
+
+btn.addEventListener("click", ()=>{
+  btnPress.innerHTML = ++pressedCount;
+
+  debouncedCount();
+})
+// roadsidecoder yt - you can done this above example using lodash library, button press debounce using lodash library
+
+
+
+
+
+
+
+
+
+/************************************************ Throttling ******************************************************************** */
+
+// Doc- https://www.freecodecamp.org/news/throttling-in-javascript/
+// Doc- https://dev.to/jeetvora331/throttling-in-javascript-easiest-explanation-1081
+// Throttling is a technique used to limit the rate at which a function is called. Throttling transforms a function such that it can only be called once in a specific interval of time.
+// Throttling is a technique that limits how often a function can be called in a given period of time. It is useful for improving the performance and responsiveness of web pages that have event listeners that trigger heavy or expensive operations, such as animations, scrolling, resizing, fetching data, etc.
+// For example, if you have a function that fetches some data from an API every time the user scrolls the page, you might want to throttle it so that it only makes one request every second, instead of making hundreds of requests as the user scrolls. This way, you can avoid overloading the server or the browser with unnecessary requests and reduce the bandwidth consumption.
+
+
+// roadsidecoder- yt
+
+const btn2 = document.querySelector(".increment_btn2");
+const btnPress2 = document.querySelector(".increment_pressed2");
+const count2 = document.querySelector(".increment_count2");
+
+var pressedCount2 =0;
+var triggerCount2 =0;
+
+const start = new Date().getTime();
+
+
+const myThrottle = (cb,d)=>{
+  // let last = 0;
+  // return(...args)=>{
+  //    let now = new Date().getTime();
+
+  //    if(now-last < d) return;
+
+  //    last = now;
+  //    return cb(...args);
+  // }
+
+  //using settimeout 
+  let timeout = null;
+  console.log('timeout', timeout);
+  return(...args)=>{
+      if (timeout) {
+          console.log('timeouttt', timeout);
+          return;
+        }
+      cb(...args);
+      timeout = setTimeout(() => {
+          timeout = null;
+        }, d);
+  }
+}
+
+var throttled = myThrottle(()=>{
+     triggerCount2 += 1;
+     count2.innerHTML = triggerCount2;
+}, 1000)
+
+btn2.addEventListener("click", ()=>{
+  btnPress2.innerHTML = ++pressedCount2;
+  
+  // const now = new Date().getTime();
+  // const seconds = (now-start) / 1000;
+  // console.log(seconds.toFixed());
+
+  throttled();
+})
+
+// roadsidecoder yt - you can done this above example using lodash library, button press debounce using lodash library
